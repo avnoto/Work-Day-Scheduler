3e8a90a1ee04b2990e590090c5254705da9a53d4
avnoto <alexandravnoto@gmail.com>
2020-05-02 21:00:33 -0700

    saves user descriptions, need to add comments still

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
-let currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+$(document).ready(function () {
 
-$("#currentDay").text(currentDay);
+doInit();
+
+function doInit() {
+    currentDay();
+    currentTime();
+    retrieveText();
+
+}
+
+function currentDay() {
+    let currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").text(currentDay);
+    };
+
+
+function currentTime() {
+
+    let currentTime = moment().hours();
+    let message = $(".message");
+
+    $(message).each(function (index, element) {
+
+    let timeBlock = parseInt($(element).attr("data-time"));
+
+    if (timeBlock < currentTime) {
+        $(element).addClass("past");
+    }
+
+    else if (timeBlock == currentTime) {
+        $(element).addClass("present");
+    }
+
+    else {
+        $(element).addClass("future");
+     }
+
+});
+}
+
+function retrieveText() {
+
+    const message = $(".message");
+    $(message).each(function(i, element){
+
+        const timeID = $(element).attr("data-time");
+        const message = localStorage.getItem(timeID);
+
+        $(element).text(message);
+
+
+    });
+
+}
+
+
+$(".saveBtn").on("click", function () {
+    const timeID = $(this).siblings(".message").attr("data-time");
+    const userInput = $(this).siblings(".message").val();
+    
+    localStorage.setItem(timeID, userInput);
+    
+    
+});
 
 
 
 
 
 
-
-
+});
